6cd5329ef1cd879c8950d8eb2e18129a22e04260
Leandro Hespanhol <leandrohespanhol@gmail.com>
2021-12-08 18:29:10 -0300

    req20-21

--- src/pages/ReceitasFeitas.js
+++ src/pages/ReceitasFeitas.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 export default function ReceitasFeitas() {
   return (
     <div>
       <Header title="Receitas Feitas" buttonDisable />
-      <Footer />
     </div>
   );
 }
