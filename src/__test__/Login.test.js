9e904280c1ada2bab2d13d90e523614c9036509c
Leandro <play3rtox1c@gmail.com>
2021-12-08 15:33:12 -0300

    teste tela de login

--- src/__test__/Login.test.js
+++ src/__test__/Login.test.js
@@ -1,8 +1,35 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 
 describe('Testa a pagina de Login', () => {
-  render(<App />);
+  it('testa se ao digitar somente o email da incorreto', () => {
+    render(<App />);
+    const email = screen.getByTestId('email-input');
+    userEvent.type(email, 'p@p.com');
 
+    const button = screen.getByRole('button', { name: 'Entrar' });
+    expect(button.disabled).toBe(true);
+  });
+
+  it('testa se ao digitar somente a senha da incorreto', () => {
+    render(<App />);
+    const password = screen.getByTestId('password-input');
+    userEvent.type(password, 'aaaaaaa');
+
+    const button = screen.getByTestId('login-submit-btn');
+    expect(button.disabled).toBe(true);
+  });
+
+  it('testa se ao digitar um login correto funciona', () => {
+    render(<App />);
+    const email = screen.getByTestId('email-input');
+    const password = screen.getByTestId('password-input');
+    userEvent.type(email, 'p@p.com');
+    userEvent.type(password, 'aaaaaaa');
+
+    const button = screen.getByTestId('login-submit-btn');
+    expect(button.disabled).toBe(false);
+  });
 });
