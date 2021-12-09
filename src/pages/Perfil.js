a859ec91b1c9313c001f647bd8224d73a5d8fbe0
Leonardo Ramos <oliveira.leonardo3004@gmail.com>
2021-12-08 22:35:11 -0300

    finaliza requisitos 82 ao 87

--- src/pages/Perfil.js
+++ src/pages/Perfil.js
@@ -1,11 +1,44 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import { getItemFromLocalStorage } from '../services/funcs';
 
 export default function Perfil() {
+  const user = getItemFromLocalStorage('user');
   return (
     <div>
       <Header title="Perfil" buttonDisable />
+      <h3 data-testid="profile-email">{ user.email }</h3>
+
+      <Link to="../receitas-feitas">
+        <button
+          type="button"
+          data-testid="profile-done-btn"
+        >
+          Receitas Feitas
+        </button>
+      </Link>
+
+      <Link to="../receitas-favoritas">
+        <button
+          type="button"
+          data-testid="profile-favorite-btn"
+        >
+          Receitas Favoritas
+        </button>
+      </Link>
+
+      <Link to="../">
+        <button
+          type="button"
+          data-testid="profile-logout-btn"
+          onClick={ () => localStorage.clear() }
+        >
+          Sair
+        </button>
+      </Link>
+
       <Footer />
     </div>
   );
