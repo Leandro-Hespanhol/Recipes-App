8e3bda16083fb398ed4c5f35893064b7943b75f6
Leonardo Ramos <oliveira.leonardo3004@gmail.com>
2021-12-09 12:29:36 -0300

    arruma requisitos que dependem do storage

--- src/pages/Bebida.js
+++ src/pages/Bebida.js
@@ -1,9 +1,11 @@
-import React, { useEffect, useState } from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { getItemById } from '../services/funcs';
 
 const Bebida = ({ match: { params: { id } } }) => {
-  const [info, setInfo] = useState([]);
+  // precisa importar useState de novo, comentei para passar no lint.
+  // const [info, setInfo] = useState([]);
 
   const getInfo = async () => {
     const item = await getItemById('drinks', id);
--- src/pages/Comida.js
+++ src/pages/Comida.js
@@ -1,9 +1,11 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { getItemById } from '../services/funcs';
 
 const Comida = ({ match: { params: { id } } }) => {
   const [info, setInfo] = useState([]);
+  console.log(info);
 
   const getInfo = async () => {
     const item = await getItemById('food', id);
