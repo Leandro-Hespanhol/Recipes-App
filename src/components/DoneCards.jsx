92ad551f3983500f016fac928fd1d038557d0e63
Leandro <play3rtox1c@gmail.com>
2021-12-13 11:53:31 -0300

    add favorite page

--- src/components/DoneCards.jsx
+++ src/components/DoneCards.jsx
@@ -2,30 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import DoneCard from './DoneCard';
 
-const DoneCards = ({ info }) => {
-  const cards = info.map(({
-    id,
-    image,
-    name,
-    category,
-    doneDate,
-    area,
-    alcoholicOrNot,
-    tags,
-    type,
-  }, index) => (
-    <Link key={ `${name}-done ${index}` } to={ `/${type}s/${id}` }>
+const DoneCards = ({ info, setInfo }) => {
+  const cards = info.map((obj, index) => (
+    <Link key={ `${obj.name}-done ${index}` } to={ `/${obj.type}s/${obj.id}` }>
       <DoneCard
-        key={ `${name}-done ${index}` }
-        id={ id }
-        image={ image }
-        area={ area }
-        name={ name }
-        alcoholicOrNot={ alcoholicOrNot }
-        type={ type }
-        category={ category }
-        date={ doneDate }
-        tags={ tags }
+        key={ `${obj.name}-done ${index}` }
+        { ...obj }
+        info={ info }
+        setInfo={ setInfo }
         index={ index }
       />
     </Link>
